172207c06f59cd59693b0e845ff0e414d35c2219
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-01 15:46:56 +0530

    first code run

--- Day-1/App.js
+++ Day-1/App.js
@@ -1,2 +1,7 @@
 var age = 22;
 console.log(age);
+function greet(name) {
+    console.log("Hello, ".concat(name, "..."));
+}
+var fullName = "Himanshu Sharma";
+greet(fullName);
--- Day-1/App.ts
+++ Day-1/App.ts
@@ -1,2 +1,12 @@
 let age = 22;
 console.log(age);
+
+
+function greet(name:String)
+{
+    console.log(`Hello, ${name}...`);   
+}
+
+let fullName:String = "Himanshu Sharma";
+
+greet(fullName);
