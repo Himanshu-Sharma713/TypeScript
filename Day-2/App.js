f0a781107bb84770707bf74a7cae1e376bbade31
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-02 12:26:40 +0530

    html file use with ts code

---
+++ Day-2/App.js
@@ -0,0 +1,4 @@
+var message = "Learn TypeScript";
+for (var i = 0; i < 10; i++) {
+    console.log("".concat(message, " ====>>>> ").concat(i + 1));
+}
---
+++ Day-2/App.ts
@@ -0,0 +1,5 @@
+let message: String = "Learn TypeScript";
+
+for (let i = 0; i < 10; i++) {
+  console.log(`${message} ====>>>> ${i + 1}`);
+}
