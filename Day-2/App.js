430bf4f183c3964a5daaf320b57c9759da6afcda
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-02 14:54:21 +0530

    watch methed in TS

--- Day-2/App.js
+++ Day-2/App.js
@@ -2,3 +2,5 @@ var message = "Learn TypeScript";
 for (var i = 0; i < 10; i++) {
     console.log("".concat(message, " ====>>>> ").concat(i + 1));
 }
+var a = 713;
+console.log("Number: ", a);
--- Day-2/App.ts
+++ Day-2/App.ts
@@ -3,3 +3,9 @@ let message: String = "Learn TypeScript";
 for (let i = 0; i < 10; i++) {
   console.log(`${message} ====>>>> ${i + 1}`);
 }
+
+let a: number = 713;
+
+console.log("Number: ",a);
+
+ 
