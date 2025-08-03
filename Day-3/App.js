d4ac55fb044b6706e7c3da1148c45ac90538ace7
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-03 12:11:56 +0530

    String data type

---
+++ Day-3/App.js
@@ -0,0 +1,7 @@
+// Number Data Types in TypeScript...
+var age = 22;
+console.log("Age: ", age);
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
---
+++ Day-3/App.ts
@@ -0,0 +1,11 @@
+// Number Data Types in TypeScript...
+
+let age : number = 22;
+console.log("Age: ",age);
+  
+var a : number = 10;
+var b : number = 20;
+
+var c : number = a + b;
+
+console.log(c);
