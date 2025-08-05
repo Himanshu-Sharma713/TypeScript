4ada026f539f3e697e71f3b05f38f81da3fb6477
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-05 15:17:23 +0530

    array completed

--- Day-5/Array.js
+++ Day-5/Array.js
@@ -1,2 +1,12 @@
 // ========= Array DataType in TypeScript ==========
 document.writeln("<h1>Array DataType in TypeScript</h1>");
+// using Square Brackets
+var fruits1 = ["Apple", "Mongo", "Orange", "Litche"];
+for (var i = 0; i < fruits1.length; i++) {
+    console.log(fruits1[i]);
+}
+// using Generic array type
+var fruits2 = ["Apple", "Mongo", "Orange", "Litche"];
+for (var i = 0; i < fruits2.length; i++) {
+    console.log(fruits2[i]);
+}
--- Day-5/Array.ts
+++ Day-5/Array.ts
@@ -2,3 +2,16 @@
 
 document.writeln("<h1>Array DataType in TypeScript</h1>");
 
+// using Square Brackets
+let fruits1: String[] = ["Apple", "Mongo", "Orange", "Litche"];
+
+for (let i = 0; i < fruits1.length; i++) {
+  console.log(fruits1[i]);
+}
+
+// using Generic array type
+let fruits2: Array<String> = ["Apple", "Mongo", "Orange", "Litche"];
+
+for (let i = 0; i < fruits2.length; i++) {
+  console.log(fruits2[i]);
+}
