9dac66f5f8a5b4872762e7ba8d18792a06e1a47f
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-06 22:16:53 +0530

    setup day-6

---
+++ Day-6/tuple.js
@@ -0,0 +1,5 @@
+document.writeln("<h1>Tuple in TypeScript</h1>");
+// Tuple in TypeScript
+var person = ["Himanshu Sharma", 22];
+console.log(person[0]);
+console.log(person[1]);
---
+++ Day-6/tuple.ts
@@ -0,0 +1,8 @@
+document.writeln("<h1>Tuple in TypeScript</h1>");
+
+// Tuple in TypeScript
+
+let person: [String, number] = ["Himanshu Sharma", 22];
+
+console.log(person[0]);
+console.log(person[1]);
