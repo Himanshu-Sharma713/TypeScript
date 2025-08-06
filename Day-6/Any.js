922c9435bdbeeb69bf0cbec837d2d059a97e42f7
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-06 22:44:08 +0530

    any completed

---
+++ Day-6/Any.js
@@ -0,0 +1,13 @@
+document.writeln("<h1>Any in TypeScript</h1>");
+// Any Data Types in TypeScript
+// ===>>> 1. Dynamic Value Assignment
+var data;
+data = "Hello";
+console.log(data); // "Hello"
+data = 42;
+console.log(data); // 42
+data = true;
+console.log(data); // true
+// ===>>> 2. Storing Mixed-Type Array
+var mixedArray = [1, "TypeScript", true, { name: "Himanshu" }];
+console.log(mixedArray);
