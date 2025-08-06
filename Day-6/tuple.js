7eeb900f75d44901828a61d068b398217dc43884
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-06 22:39:38 +0530

    tuple comleted

--- Day-6/tuple.js
+++ Day-6/tuple.js
@@ -1,5 +1,15 @@
 document.writeln("<h1>Tuple in TypeScript</h1>");
 // Tuple in TypeScript
+// ===> Example 1
 var person = ["Himanshu Sharma", 22];
 console.log(person[0]);
 console.log(person[1]);
+// ===> Example 2
+var employee = [101, "John", true];
+console.log("ID: ".concat(employee[0], ", Name: ").concat(employee[1], ", Active: ").concat(employee[2]));
+// ===> Example 3
+function getCoordinates() {
+    return [10, 20];
+}
+var _a = getCoordinates(), x = _a[0], y = _a[1];
+console.log("X: ".concat(x, ", Y: ").concat(y));
--- Day-6/tuple.ts
+++ Day-6/tuple.ts
@@ -2,7 +2,25 @@ document.writeln("<h1>Tuple in TypeScript</h1>");
 
 // Tuple in TypeScript
 
+// ===> Example 1
+
 let person: [String, number] = ["Himanshu Sharma", 22];
 
 console.log(person[0]);
 console.log(person[1]);
+
+// ===> Example 2
+
+let employee: [number, string, boolean] = [101, "John", true];
+
+console.log(`ID: ${employee[0]}, Name: ${employee[1]}, Active: ${employee[2]}`);
+
+// ===> Example 3
+
+function getCoordinates(): [number, number] {
+  return [10, 20];
+}
+
+const [x, y] = getCoordinates();
+console.log(`X: ${x}, Y: ${y}`);
+
