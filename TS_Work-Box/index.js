f860c0bb8fb5f6132692e01c9a535fdf09af7ad6
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-13 06:53:57 +0530

    Revision of Typescript...

---
+++ TS_Work-Box/index.js
@@ -0,0 +1,4 @@
+var fullName = "Himanshu Sharma";
+var age = 22;
+console.log("Name:  ", fullName);
+console.log("Age:  ", age);
---
+++ TS_Work-Box/index.ts
@@ -0,0 +1,5 @@
+let fullName: String = "Himanshu Sharma";
+let age: number = 22;
+
+console.log("Name:  ", fullName);
+console.log("Age:  ", age);
