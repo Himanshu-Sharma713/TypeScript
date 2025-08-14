3ae3da961571418e9721f68c125b5089df0434bb
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-14 22:39:44 +0530

    complete TypeScript

---
+++ TS_One/inputs.js
@@ -0,0 +1,8 @@
+function getInfo() {
+    var userName = document.getElementById("username");
+    console.log(userName.value);
+    var email = document.getElementById("emali");
+    console.log(email.value);
+    var age = document.getElementById("age");
+    console.log(age.value);
+}
---
+++ TS_One/inputs.ts
@@ -0,0 +1,8 @@
+function getInfo() {
+    const userName = document.getElementById("username") as HTMLInputElement;
+    console.log(userName.value);
+    const email = document.getElementById("emali") as HTMLInputElement;
+    console.log(email.value);
+    const age = document.getElementById("age") as HTMLInputElement;
+    console.log(age.value);
+}
